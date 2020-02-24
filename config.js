cbdbda5f8e80696fe7223d74d3aec382234e9583
stephmen <stephmen@gmail.com>
2020-02-24 17:01:12 -0500

    Changing IP address adding http://

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 //export const endpoint = `http://localhost:4444`;
-export const endpoint = `172.30.64.184:4444`;
+export const endpoint = `http://172.30.64.184:4444`;
 
 export const perPage = 4;
