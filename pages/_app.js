2f9e121f1211ed1cf91950bea2cfae4b8fdb69ff
stephmen <stephmen@gmail.com>
2020-02-25 16:36:48 -0500

    adding <ApolloProvider client={apollo}>

--- pages/_app.js
+++ pages/_app.js
@@ -15,16 +15,16 @@ class MyApp extends App {
     return { pageProps };
   }
   render() {
-    // const { Component, apollo, pageProps } = this.props;
+    const { Component, apollo, pageProps } = this.props;
     const { Component, pageProps } = this.props;
     return (
       
       //added ApolloProvider from wes Boss
-      // <ApolloProvider client={apollo}>
+      <ApolloProvider client={apollo}>
         <Page>
           <Component {...pageProps} />
         </Page>
-      // </ApolloProvider>
+      </ApolloProvider>
     );
   }
 }
