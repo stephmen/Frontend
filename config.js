be3fa7538fecb7270e0810e1831de961b04356b8
stephmen <stephmen@gmail.com>
2020-02-28 19:10:52 -0500

    Changed IP adress

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `http://localhost:4444`;
-//export const endpoint = `http://172.30.64.184:4444`;
+//export const endpoint = `http://localhost:4444`;
+//export const prodEndpoint = `http://localhost:4444`;
+export const endpoint = `http://172.30.64.184:4444`;
 
 export const perPage = 4;
