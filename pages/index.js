7d2b036dee5db9ced29f1698ca5647865d5b0a14
stephmen <stephmen@gmail.com>
2020-03-03 14:04:25 -0500

    page error temporay fix for server

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Items from "../components/Items";
 import withApollo from "../lib/nextApollo";
 
 const Home = props => {
-  if (props.query.page === "undefined") {
+  // if (props.query.page === "undefined") {
     props = { query: { page: 1 } };
-  }
+  // }
   return (
     <div>
       <Items page={parseFloat(props.query.page) || 1} />
