83b88a248f445ac6d6690e8b49f1a429763025f0
stephmen <stephmen@gmail.com>
2020-03-06 09:08:19 -0500

    Changing URL to Absolute

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 //export const endpoint = `http://localhost:4444`;
 //export const prodEndpoint = `http://localhost:4444`;
-export const prodEndpoint = `apollo-backend-myeccomv002.apps-crc.testing`;
-export const endpoint = `apollo-backend-myeccomv002.apps-crc.testing`;
+export const prodEndpoint = `http://apollo-backend-myeccomv002.apps-crc.testing`;
+export const endpoint = `http://apollo-backend-myeccomv002.apps-crc.testing`;
 
 export const perPage = 4;
