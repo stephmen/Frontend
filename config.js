fe7fa26c11895d289d6d575b4533b410e8116821
stephmen <stephmen@gmail.com>
2020-03-24 22:08:34 -0400

    Changing IP Address to 172.30.212.146:4444

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 //export const endpoint = `http://localhost:4444`;
 //export const prodEndpoint = `http://localhost:4444`;
-export const prodEndpoint = `http://apollo-backend-myeccomv002.apps-crc.testing`;
-export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = `http://apollo-backend-myeccomv003.apps-crc.testing`;
+export const endpoint = `http://172.30.212.146:4444`;
 
 export const perPage = 4;
