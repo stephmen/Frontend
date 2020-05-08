15473e0be2930c3e27fa6218b471599c839d1f2d
stephmen <stephmen@gmail.com>
2020-05-07 21:50:48 -0400

    Adapting Appication for cloud

--- config.js
+++ config.js
@@ -1,7 +1,16 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 //export const endpoint = `http://localhost:4444`;
 //export const prodEndpoint = `http://localhost:4444`;
-export const prodEndpoint = `http://apollo-backend-myeccomv003.apps-crc.testing`;
-export const endpoint = `http://172.30.212.146:4444`;
+
+// Before working with environment variables:
+// export const prodEndpoint = `http://apollo-backend-myeccomv003.apps-crc.testing`;
+// export const endpoint = `http://172.30.212.146:4444`;
+
+//working with environment Variables
+
+export const prodEndpoint = process.env.$PRODENPOINT
+export const endpoint = process.env.$ENDPOINT 
+
+
 
 export const perPage = 4;
