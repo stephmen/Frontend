bc89068caa98aa609f30e8e443929d41c16068e6
stephmen <stephmen@gmail.com>
2020-05-13 11:56:30 -0400

    testing console.log(process.env.endpoint)

--- components/Items.js
+++ components/Items.js
@@ -44,6 +44,7 @@ const Items = (props) => {
     onCompleted: data => { }
   });
   console.log({ ...data });
+  console.log(process.env.endpoint)
   if (loading && !data) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   return (
