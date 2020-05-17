3bcd194bf913d305f1c13c11a0e3f9a4bd5b1dfb
stephmen <stephmen@gmail.com>
2020-05-17 08:27:54 -0400

    config.js

--- config.js
+++ config.js
@@ -8,8 +8,10 @@
 
 //working with environment Variables
 
-export const prodEndpoint = process.env.prodEndpoint
+export const prodEndpoint = "http://172.30.254.196:4444"
 export const endpoint = process.env.endpoint 
+// export const prodEndpoint = process.env.prodEndpoint
+// export const endpoint = process.env.endpoint 
 
 
 
