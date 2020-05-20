13ca3dd2c19f27378aa4b4cb56d0cf4424205f02
stephmen <stephmen@gmail.com>
2020-05-20 11:39:24 -0400

    changing path dotenc { path: '/custom/path/to/.env' }

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: '/config/.env' })
 module.exports = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
