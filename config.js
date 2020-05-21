86acabe5f0188f67c2472dc72f6870b2e6fdf1fb
stephmen <stephmen@gmail.com>
2020-05-21 13:02:10 -0400

    .env config working

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-//export const endpoint = `http://localhost:4444`;
 //export const prodEndpoint = `http://localhost:4444`;
 
 // Before working with environment variables:
@@ -9,11 +8,13 @@
 //working with environment Variables
 
 //export const prodEndpoint = "http://172.30.254.196:4444"
-export const prodEndpoint = `http://apollo-backend-development.apps-crc.testing`;
+//export const prodEndpoint = `http://apollo-backend-development.apps-crc.testing`;
 //export const prodEndpoint = process.env.prodEndpoint
-export const endpoint = process.env.endpoint 
 // export const prodEndpoint = process.env.prodEndpoint
 // export const endpoint = process.env.endpoint 
+
+export const endpoint = process.env.ENDPOINT
+export const prodEndpoint = process.env.PRODENDPOINT;
 
 
 
--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-require('dotenv').config({ path: '/config/.env' })
+require('dotenv').config({ path: './config/.env' })
 module.exports = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     TEST_VAR: process.env.TEST_VAR,
-    prodEndpoint: process.env.prodEndpoint,
-    endpoint: process.env.endpoint 
+    PRODENDPOINT: process.env.PRODENDPOINT,
+    ENDPOINT: process.env.ENDPOINT
   },
 }
