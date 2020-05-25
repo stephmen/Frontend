ebfb4a9cb658abc38a75e286a9f32fabd6ca9c8a
stephmen <stephmen@gmail.com>
2020-05-25 17:18:40 -0400

    changing back to .env

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-//require('dotenv').config({ path: './config/dev.env' })
+require('dotenv').config({ path: './config/dev.env' })
 module.exports = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     PRODENDPOINT: process.env.PRODENDPOINT,
     ENDPOINT: process.env.ENDPOINT,
     STAGE: process.env.STAGE,
-    APOLLO_TEST: process.env.APOLLO_BACKEND_PORT_4444_TCP,
+    APOLLO_TEST: process.env.APOLLO_BACKEND_PORT_4444_TCP,  
     TEST_VAR: process.env.TEST_VAR,
   },
 }
