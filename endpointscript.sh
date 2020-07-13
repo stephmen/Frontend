478c68e5adbba063b70878129e677aa62dc80d19
stephmen <stephmen@gmail.com>
2020-07-13 18:43:31 -0400

    Last Test

--- endpointscript.sh
+++ endpointscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Creating dev.env
-touch config/dev.env
-chmod 777 config/dev.env
+touch dev.env
+chmod 777 dev.env
 echo checking dev.env
-ls -all config/dev.env
+ls -all dev.env
 echo This is the Apollo Server endpoint:
 echo PRODENDPOINT='"'$PRODENDPOINT'"'
 echo PRODENDPOINT='"'$PRODENDPOINT'"' >> config/dev.env
