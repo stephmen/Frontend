b4208b6a167143de63de4f6f3c9ef47423a69b9a
stephmen <stephmen@gmail.com>
2021-01-24 12:54:53 -0500

    Fixing Endpoint in config/dev.env

--- OpenshiftFiles/Project/development/createvolumedevenv.sh
+++ OpenshiftFiles/Project/development/createvolumedevenv.sh
@@ -1,4 +1,2 @@
-oc set volume deploymentconfig/frontend --add --name=devconfig --mount-path=/opt/app-root/src/config --type=configmap --configmap-name=dev-env-configmap
-oc set volume deploymentconfig/frontend --add --name=test-config --mount-path=/opt/app-root/src/config --type=configmap --configmap-name=app-config
-
-MountVolume.SetUp failed for volume "app-config" : configmap "test-config
+oc set volume deployment/frontend --add --name=devconfig --mount-path=/opt/app-root/src/config --type=configmap --configmap-name=dev-env-configmap
+oc set volume deployment/frontend --add --name=test-config --mount-path=/opt/app-root/src/config --type=configmap --configmap-name=app-config
