230aa5e91dfed85c511064fc2c8dd776036f8c60
stephmen <stephmen@gmail.com>
2022-10-18 13:56:16 -0400

    Tuesday Oct 18 Commiting from while

--- Dockerfile
+++ Dockerfile
@@ -5,16 +5,19 @@ WORKDIR /usr/src/frontend/
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
 COPY package*.json /usr/src/frontend/
-
+ENV PRODENDPOINT myeccom.com:4444
+ENV ENDPOINT myeccom.com:4444
 #RUN npm install
 # If you are building your code for production
 RUN npm ci --only=production
 COPY . .
 RUN npm install
 RUN chmod a+x endpointscript.sh
-#RUN npm run build
-#RUN chmod 777 /usr/src/frontend/.next/BUILD_ID
+RUN npm run build
+RUN chmod 777 /usr/src/frontend/.next/BUILD_ID
 EXPOSE 7777
+# this is for kubernetes CMD [ "npm", "run", "start" ]
+#CMD [ "npm", "run", "dev" ]
 CMD [ "npm", "run", "start" ]
 
 ####The line below was used to create the container
--- config.js
+++ config.js
@@ -1,22 +1,27 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-//export const prodEndpoint = `http://localhost:4444`;
+// export const prodEndpoint = `http://localhost:4444`;
 
 // Before working with environment variables:
 // export const prodEndpoint = `http://apollo-backend-myeccomv003.apps-crc.testing`;
 // export const endpoint = `http://172.30.212.146:4444`;
 
-//working with environment Variables
+// working with environment Variables
 
-//export const prodEndpoint = "http://172.30.254.196:4444"
-//export const prodEndpoint = `http://apollo-backend-development.apps-crc.testing`;
-//export const prodEndpoint = process.env.prodEndpoint
+// export const prodEndpoint = "http://172.30.254.196:4444"
+// export const prodEndpoint = `http://apollo-backend-development.apps-crc.testing`;
+// export const prodEndpoint = process.env.prodEndpoint
 // export const prodEndpoint = process.env.prodEndpoint
 // export const endpoint = process.env.endpoint 
 
+
+// K8 Config
 export const endpoint = process.env.ENDPOINT
 export const prodEndpoint = process.env.PRODENDPOINT;
 export const stage = process.env.STAGE;
 
+
+//For Local Usage only
+//export const endpoint = `http://myeccom.com:4444`;
 
 
 export const perPage = 4;
